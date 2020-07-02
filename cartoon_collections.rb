af130d97f369513b527f6fc4700474f099aa3cfc
Ashlee Barksdale <ashleelennab@gmail.com>
2020-07-02 06:22:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   
-  array.each do |array|
-    puts "Hello {array}!"
+  array.each do |arrays|
+    puts "Hello {arrays}!"
   end
 end
 
