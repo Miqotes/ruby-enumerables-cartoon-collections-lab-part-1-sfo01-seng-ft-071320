cc88a2bcbaf6b77d39863adf9e45120f4ea510ae
Ashlee Barksdale <ashleelennab@gmail.com>
2020-07-02 06:38:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |arrays, index|
-    puts "#{arrays}"
+    puts "#{arrays[index].next}"
   end
 end
